5ba02724a5980ed7d27365947d66ef53cd216276
Abdulrahman Mohamed <AbdulrahmanMo124@gmail.com>
2024-07-30 06:53:54 +0300

    every thing ok except showing palgarism

--- src/Api/checkPalgarism.js
+++ src/Api/checkPalgarism.js
@@ -8,7 +8,7 @@ const checkPalgarism = async (description) => {
     }
     const { token } = JSON.parse(tokenData);
 
-    // API endpoint
+    // API Endpoint
     //const url = 'http://127.0.0.1:7000/similarity';
     const url = 'http://127.0.0.1:8000/api/admin/check_plagiarism';
     const formData = new FormData();
