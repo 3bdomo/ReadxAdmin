7c364b9e2770d3d1dee86b2eca955355c8d141b7
Abdulrahman Mohamed <AbdulrahmanMo124@gmail.com>
2024-09-30 02:53:57 +0300

    test infinty free

--- src/Api/baseUrl.js
+++ src/Api/baseUrl.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const baseURL = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  // baseURL: "http://127.0.0.1:8000",
+  baseURL: "http://readx.free.nf/laravel/public/",
   withCredentials: true, // Ensure credentials are included in requests
   headers: {
     "Content-Type": "application/json",
