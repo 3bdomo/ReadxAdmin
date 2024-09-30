ccc47d803201e226cfb81202cf6fc4dbe714ea16
Abdulrahman Mohamed <AbdulrahmanMo124@gmail.com>
2024-09-30 18:04:00 +0300

    set base url localhost

--- src/Api/baseUrl.js
+++ src/Api/baseUrl.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const baseURL = axios.create({
-  // baseURL: "http://127.0.0.1:8000",
-  baseURL: "https://readx.free.nf/laravel/public/",
+  baseURL: "http://127.0.0.1:8000",
+  // baseURL: "https://readx.free.nf/laravel/public/",
   withCredentials: true, // Ensure credentials are included in requests
   headers: {
     "Content-Type": "application/json",
